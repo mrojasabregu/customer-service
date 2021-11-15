8355e6a2e2f3eff215451dbc16d68bea5650b249
Yanina-31 <ypiredda31@gmail.com>
2021-11-15 17:52:45 -0300

    Inicializando Entidades

--- customerService/src/main/java/com/example/customer/customerService/Domain/Model/Address.java
+++ customerService/src/main/java/com/example/customer/customerService/Domain/Model/Address.java
@@ -1,4 +1,20 @@
 package com.example.customer.customerService.Domain.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class Address {
+    private UUID idAddress;
+    private String city;
+    private String street;
+    private Long streetNumber;
+    private Long floor;
+    private String flat;
 }
--- customerService/src/main/java/com/example/customer/customerService/Domain/Model/Customer.java
+++ customerService/src/main/java/com/example/customer/customerService/Domain/Model/Customer.java
@@ -1,4 +1,22 @@
 package com.example.customer.customerService.Domain.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class Customer {
+    private UUID idCustomer;
+    private String name;
+    private String lastName;
+    private String type;
+    private Long documentNumber;
+    private String email;
+    private Long phone;
+    private Address address;
 }
