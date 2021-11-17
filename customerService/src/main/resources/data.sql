8e15e9450ef632947c177cdd5568fcef6e3b2acf
germansanz93 <germansanz93@gmail.com>
2021-11-17 15:42:22 -0300

    application.yaml data.sql

---
+++ customerService/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS address;
+
+CREATE TABLE customer(
+  id_customer VARCHAR(255) NOT NULL PRIMARY KEY,
+  name VARCHAR(255),
+  last_name VARCHAR(255),
+  document_type VARCHAR(10),
+  document_number BIGINT,
+  email VARCHAR(255),
+  phone VARCHAR(255)
+);
+
+CREATE TABLE address(
+  id_address VARCHAR(255) NOT NULL PRIMARY KEY,
+  city VARCHAR(255),
+  street VARCHAR(255),
+  street_number INT,
+  floor_number INT,
+  flat VARCHAR(255),
+  id_customer BINARY(16)
+);
