add82f691be9b5d1c79587b5b5e936a174b0caba
germansanz93 <germansanz93@gmail.com>
2021-11-18 02:53:48 -0300

    refactoring 3

--- customerService/src/main/java/com/example/customer/customerService/Service/Interface/IAddressService.java
+++ customerService/src/main/java/com/example/customer/customerService/Service/Interface/IAddressService.java
@@ -1,8 +1,4 @@
 package com.example.customer.customerService.Service.Interface;
 
-<<<<<<< Updated upstream:src/main/java/com/example/customer/customerService/Service/Interface/IServiceAddress.java
-public interface IServiceAddress {
-=======
 public interface IAddressService {
->>>>>>> Stashed changes:customerService/src/main/java/com/example/customer/customerService/Service/Interface/IAddressService.java
 }
--- customerService/src/main/java/com/example/customer/customerService/Service/Interface/ICustomerService.java
+++ customerService/src/main/java/com/example/customer/customerService/Service/Interface/ICustomerService.java
@@ -1,8 +1,5 @@
 package com.example.customer.customerService.Service.Interface;
 
-<<<<<<< Updated upstream:src/main/java/com/example/customer/customerService/Service/Interface/IServiceCustomer.java
-public interface IServiceCustomer {
-=======
 public interface ICustomerService {
->>>>>>> Stashed changes:customerService/src/main/java/com/example/customer/customerService/Service/Interface/ICustomerService.java
+
 }
